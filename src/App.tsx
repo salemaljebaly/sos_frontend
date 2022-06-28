a8982052ee5b06647a6502f14970098e12888a17
salemaljebaly <salemaljebaly@gmail.com>
2022-06-28 14:34:17 +0200

    fix image url

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import MiniDrawer from "./components/drawer2";
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignIn from "./pages/users/signIn";
 function App() {
-  // desctruct memebers from user state [ userSlice]
   const { user, isError, isSucces, isLoading, message } = useSelector(
     (state: any) => state.auth
   );
